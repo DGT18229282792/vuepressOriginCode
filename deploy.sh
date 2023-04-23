ccecda59ebbe431fbf125cb39e4b936338908705
DGT <1129180121@qq.com>
2023-04-23 17:22:02 +0800

    '修改源码', Author: DGT, Time: 2023/04/23 17:22:01

--- deploy.sh
+++ deploy.sh
@@ -10,24 +10,23 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
+# 当前时间
 export current_time=$(date "+%Y/%m/%d %H:%M:%S")
-
+# 在打包信息中上传当前时间以及作者信息
 git commit -m "Your commit message, Author: $(git config user.name), Time: $current_time"
-
-# git commit -m '2023/4/23  打包构建发布'
-
 # 推到你仓库的的 gh-page 分支
 # 将 <USERNAME>/<REPO> 替换为你的信息
 git push -f git@github.com:DGT18229282792/studyspace.git master:gh-pages
+# 终端输出发布成功标志
+echo "已打包并发布!"
 
-echo "已打包并发布"
-
-# 返回上一级目录
+# 返回项目根目录目录
 
 cd ../../../
 
 # 执行脚本 将源代码上传到github上面
 git add .
-git commit -m '17:05 上传源代码'
+export origin_current_time=$(date "+%Y/%m/%d %H:%M:%S")
+git commit -m "'修改源码', Author: $(git config user.name), Time: $origin_current_time"
 git pull
 git push
